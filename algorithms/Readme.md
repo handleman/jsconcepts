31a915e7ee1798d8718667130a9393ba2cd2e5e6
handleman <pavel.handleman@gmail.com>
2018-10-08 16:08:52 +0300

    native array for queue data-structure

--- algorithms/Readme.md
+++ algorithms/Readme.md
@@ -1,4 +1,4 @@
-## Common algorithms comncepts made in js
+## Common algorithms concepts made in js
 
 
 Currently implemented are :
@@ -6,4 +6,5 @@ Currently implemented are :
 1. [Binary Search algorithm](./binary.js)
 1. [Fast Sorting algorithm based on Binary Search ](./fast_sorting.js)
 1. [Breadth-first search algorithm implemented in js ](./breadth_first.js)
+1. [Breadth-first search algorithm implemented in js (native Array for queue) ](./breadth_first.js)
 1. [Classical Recursion exercises implemented in js ](./recursion.js)
---
+++ algorithms/breadth_first_native.js
@@ -0,0 +1,63 @@
+const foo = {
+    pavel:{
+        friends:['olga','anton','denis','vad'],
+        sellMango: false,
+    },
+    olga:{
+        friends:['tania','denis','vad'],
+        sellMango: false,
+    },
+    vad:{
+        friends:['tania','oleg','vad'],
+        sellMango: false,
+    },
+    anton:{
+        friends:['pavel','oleg','olga'],
+        sellMango: false,
+    },
+    denis:{
+        friends:['pavel','vad','olga'],
+        sellMango: false,
+    },
+    tania:{
+        friends:['anton','denis','olga'],
+        sellMango: false,
+    },
+    oleg:{
+        friends:['anton', 'vad'],
+        sellMango:true,
+    }
+};
+
+
+
+function matchCondition(val) {
+    return val.sellMango ? true : false;
+}
+// export function breadthFirst(list, start){
+function breadthFirst(list, start){
+    const qu = [];
+    const passed = {};
+
+
+    qu.push(start);
+    passed[start] = true;
+
+    while(qu.length) {
+        const item = qu.shift();
+        passed[item] = true;
+
+        if(matchCondition(list[item])){
+            return item;
+        }else{
+            list[item].friends.forEach((val)=>{
+                if(!passed[val]){
+                    qu.push(val);
+                    passed[val] = true;
+
+                }
+            });
+        }
+    }
+}
+export {breadthFirst, foo}
