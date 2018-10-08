f2c0b542284b6781dd10eceb8eab7f1e1876b58e
handleman <pavel.handleman@gmail.com>
2018-10-08 17:34:28 +0300

    stack length property

--- data-structures/stack.js
+++ data-structures/stack.js
@@ -1,20 +1,26 @@
-class Stack{
-    constructor(){
-        this._lenght = 0;
-        this._values = {};
-    }
-    push(val){
-        this._lenght++;
-        this._values[length] = val;
-    }
-    pop(){
-        if(this._lenght){
-            const val = this._values[this._lenght];
-            delete this.values[this._lenght];
-            this._lenght--;
-            return val;
-        }
-    }
+class Stack {
+	constructor() {
+		this._lenght = 0;
+		this._values = {};
+	}
+
+	push(val) {
+		this._lenght++;
+		this._values[length] = val;
+	}
+
+	pop() {
+		if (this._lenght) {
+			const val = this._values[this._lenght];
+			delete this.values[this._lenght];
+			this._lenght--;
+			return val;
+		}
+	}
+
+	length() {
+		return this._lenght;
+	}
 }
 
 export default Stack;
