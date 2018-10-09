86ddb68db0d2ac1980113ce2a5184d0794f5b443
handleman <pavel.handleman@gmail.com>
2018-10-09 22:39:06 +0300

    fast sorting pivot duplicate bug

--- algorithms/fast_sorting.js
+++ algorithms/fast_sorting.js
@@ -1,12 +1,17 @@
 const fast_sort = (item) => {
-    if(item.length <= 2 ){
-        return item;
-    }
-    else{
-        var pivot = item[Math.floor((item.length)/2)-1];
-        var leftPart = item.filter(function(val){ return val < pivot});
-        var rightPart = item.filter(function(val){ return val > pivot});
-        return fast_sort(leftPart).concat([pivot], fast_sort(rightPart))
-    }
+	if(item && Array.isArray(item)){
+		if(item.length <= 2 ){
+			return item;
+		}
+		else{
+			var pivot = item[Math.floor((item.length)/2)-1];
+			item.splice(item.indexOf(pivot),1);
+			var leftPart = item.filter(function(val){ return val <= pivot});
+			var rightPart = item.filter(function(val){ return val > pivot});
+			return fast_sort(leftPart).concat([pivot], fast_sort(rightPart))
+		}
+	}else{
+		return null;
+	}
 };
 export default fast_sort;
