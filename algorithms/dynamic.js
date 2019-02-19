150f6b6092e1d8465a5ad49aeb879bd817cf83ec
Pavel Handleman <pavel.handleman@gmail.com>
2019-02-19 11:56:06 +0300

    test description and function scaffolding

--- algorithms/dynamic.js
+++ algorithms/dynamic.js
@@ -1,8 +1,35 @@
-
 /**
+ * should find the longest common subsequence
+ * @param {String} searchQyery string user want to find
+ * @param {Array} examples array of variants from which algorithm needs to chose
  */
 function dynamic(searchQyery, examples){
+	const query = searchQyery.split();
+	const result = {
+		matrix:[],
+		length:0,
+		subsequence:null,
+	};
 
+	examples.forEach((val)=>{
+		const example = val.split('');
+
+		for(let i =0; i < query.length; i++){
+
+			for(let j=0; j < example.length; j++){
+
+			}
+
+		}
+
+	});
+
+	/**
+	 * calculate value for each calcualtion matrix cell.
+	 */
+	function calculateWeight() {
+
+	}
 }
 
 export {dynamic}
--- algorithms/dynamic.test.js
+++ algorithms/dynamic.test.js
@@ -4,11 +4,29 @@ import chai from 'chai';
 
 const expect  = chai.expect;
 
+const examples = [
+	'goat',
+	'boat',
+	'blowout',
+	'thoughtful',
+	'treat',
+	'through',
+	'thread'
+];
 
 describe('algorithms/dynamic.js', ()=>{
+	const query1 = 'tread';
+	const query2 = 'houl';
+	const query3 = 'throwout';
 
 	it('should be defined', () => {
 		expect(dynamic).to.be.a('function');
+	});
+
+	it('should find the longest common subsequence',()=>{
+		expect(dynamic(query1, examples)).equal('thread');
+		expect(dynamic(query2, examples)).equal('thoughtful');
+		expect(dynamic(query3, examples)).equal('blowout');
 	});
 
 });
